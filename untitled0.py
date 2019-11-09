75cc06c73e5d1a17c7ebdbb73f45d28868cf2295
Josh Kim <joshyoungkim@gmail.com>
2019-11-09 14:29:02 -0500

    Added File

---
+++ untitled0.py
@@ -0,0 +1,13 @@
+
+my_info = {
+    "name":"Josh Kim",
+    "age": 31,
+    "hobbies":
+        ["golf", "kendo", "exams"],
+    "wake":
+        {"Monday":6,"Tuesday":5}
+    }
+    
+ｐｒｉｎｔ（ｍｙ＿ｉｎｆｏ）
+      
+＃Ｈｅllo my name is {my_info["name"]} and I am {my_info["age"]}＇)
